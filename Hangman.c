b912c6cc9dbf2e59ff5ef33d7199a784860ba8b4
sina1verse <158719695+sina1verse@users.noreply.github.com>
2024-02-21 05:32:55 +0330

    Update on Hangman.c . (Hangman1.1)

--- Hangman.c
+++ Hangman.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <time.h>
 
 
 
 main()
 {
-    char bWord[200];
-    char gWord[200];
+    char bWord[200], bWordCoppy[200];
+    char gChar;
     bool flag, result;
-    int wLength,i;
+    int wLength, ranNum, omittedNum, temp, n, i, j;
+    int hintList[200]={0}, ranPool[200];
+    int poop;
+    srand(time(NULL));
 
     printf("\n\t\t-----------------------\n\t\tWelcome to Hangman Game");
     printf("\n\t\t-----------------------\n\n");
@@ -20,7 +25,9 @@ main()
     printf("\n\n******************************************************\n");
 
     printf("\n ::Player 1, enter the base word:: ");
+    printf("\n Your guess: ");
 
+    // Getting base word and checking the number of words
     flag=1;
     do
     {
@@ -31,24 +38,187 @@ main()
         {printf("\nIllegal amount of letters. Try again:");}
     } while (flag);
 
-    printf(" ::Player 2, now you have to guess:: ");
-    result = 0;
-    for(i=0; i<3; i++)
+
+    // making a pool of numbers
+
+    for (i=0; i<wLength; i++)
     {
-        scanf("%s", gWord);
-        if (strcmp(bWord, gWord) == 0){
-            result=1;
-            break;
-        }
-        else {
-           if (i==2)break;
-           printf("\nWrong!!! Try again:");
+        ranPool[i]= i;
+    }
+
+    //randomizing the pool of numbers
+
+    for (i=0; i<wLength; i++)
+    {
+        j=rand() % wLength ;
+        temp = ranPool[i];
+        ranPool[i] = ranPool[j];
+        ranPool[j] = temp;
+    }
+
+
+    strcpy(bWordCoppy, bWord);
+    omittedNum = wLength*6/10;
+
+
+    for (i=0; i<omittedNum; i++)
+    {
+        bWordCoppy[ranPool[i]] = '-';
+    }
+
+    //showing the default word and prompting guesses
+
+    printf("\n\n\n ::Player 2, now you have to guess 1 character:: ");
+
+    printf("\n\n\n");
+    printf("\n:     __-------        :");
+    printf("\n:    /..\\     |        :");
+    printf("\n:    | -|     |        :");
+    printf("\n:     \\/      |        :");
+    printf("\n:             |        :");
+    printf("\n:             |        :");
+    printf("\n:             |        :");
+    printf("\n:             |        :");
+    printf("\n:            / \\       :");
+    printf("\n:           /   \\      :");
+    printf("\n:          /     \\     :");
+    printf("\n\n    \4  %s  \4\n\n",bWordCoppy);
+    printf("\n Your guess:");
+
+
+
+    result = 0;
+    getchar();
+    scanf("%c", &gChar);
+
+    for (i=0; i<omittedNum; i++)
+    {
+        if(bWord[ranPool[i]] == gChar){
+            bWordCoppy[ranPool[i]]=bWord[ranPool[i]];
+            result = 1;
         }
     }
-    (result)? printf("\n ::Player 2, you won !!!\n\n") : printf("\n ::Player 1, you won!!!\n\n");
+    if (!strcmp(bWordCoppy, bWord))
+    {
+        printf("\n ::Player 2, you won !!!");
+        printf("\n\n\n");
+    printf("\n:     __               :");
+    printf("\n:    /^^\\              :");
+    printf("\n:    | 0|              :");
+    printf("\n:     \\/               :");
+    printf("\n:     |                :");
+    printf("\n:    /|\\               :");
+    printf("\n:   / | \\              :");
+    printf("\n:     |                :");
+    printf("\n:     /\\               :");
+    printf("\n:    /  \\              :");
+    printf("\n:   ----------------   :");
+    printf("\n\n    \4  %s  \4\n\n",bWordCoppy);
+        exit(1);
+    }
+
+    printf("\n\n\n");
+    printf("\n:     __-------        :");
+    printf("\n:    /..\\     |        :");
+    printf("\n:    | -|     |        :");
+    printf("\n:     \\/      |        :");
+    printf("\n:     |       |        :");
+    printf("\n:    /|\\      |        :");
+    printf("\n:   /   \\     |        :");
+    printf("\n:             |        :");
+    printf("\n:            / \\       :");
+    printf("\n:           /   \\      :");
+    printf("\n:          /     \\     :");
+    printf("\n\n    \4  %s  \4\n\n",bWordCoppy);
+    printf("\n Your guess: ");
+
+    getchar();
+    scanf("%c", &gChar);
+    for (i=0; i<omittedNum; i++)
+    {
+        if(bWord[ranPool[i]] == gChar){
+            bWordCoppy[ranPool[i]]=bWord[ranPool[i]];
+            result = 1;
+        }
+    }
+    if (!strcmp(bWordCoppy, bWord))
+    {
+        printf("\n ::Player 2, you won !!!");
+        printf("\n\n\n");
+    printf("\n:     __               :");
+    printf("\n:    /^^\\              :");
+    printf("\n:    | 0|              :");
+    printf("\n:     \\/               :");
+    printf("\n:     |                :");
+    printf("\n:    /|\\               :");
+    printf("\n:   / | \\              :");
+    printf("\n:     |                :");
+    printf("\n:     /\\               :");
+    printf("\n:    /  \\              :");
+    printf("\n:   ----------------   :");
+    printf("\n\n    \4  %s  \4\n\n",bWordCoppy);
+        exit(1);
+    }
+
+    printf("\n\n\n");
+    printf("\n:     __-------        :");
+    printf("\n:    /..\\     |        :");
+    printf("\n:    | -|     |        :");
+    printf("\n:     \\/      |        :");
+    printf("\n:     |       |        :");
+    printf("\n:    /|\\      |        :");
+    printf("\n:   / | \\     |        :");
+    printf("\n:     |       |        :");
+    printf("\n:     /\\     / \\       :");
+    printf("\n:    /  \\   /   \\      :");
+    printf("\n:          /     \\     :");
+    printf("\n\n    \4  %s  \4\n\n",bWordCoppy);
+    printf("\n Your guess: ");
+
+    getchar();
+    scanf("%c", &gChar);
+    for (i=0; i<omittedNum; i++)
+    {
+        if(bWord[ranPool[i]] == gChar){
+            bWordCoppy[ranPool[i]]=bWord[ranPool[i]];
+            result = 1;
+        }
+    }
+    if (!strcmp(bWordCoppy, bWord))
+    {
+        printf("\n ::Player 2, you won !!!");
+        printf("\n\n\n");
+    printf("\n:     __               :");
+    printf("\n:    /^^\\              :");
+    printf("\n:    | 0|              :");
+    printf("\n:     \\/               :");
+    printf("\n:     |                :");
+    printf("\n:    /|\\               :");
+    printf("\n:   / | \\              :");
+    printf("\n:     |                :");
+    printf("\n:     /\\               :");
+    printf("\n:    /  \\              :");
+    printf("\n:   ----------------   :");
+    printf("\n\n    \4  %s  \4\n\n",bWordCoppy);
+        exit(1);
+    }
+
+    printf("\n\n\n");
+    printf("\n:     __-------        :");
+    printf("\n:    /xx\\     |        :");
+    printf("\n:    |  |     |        :");
+    printf("\n:     \\/      |        :");
+    printf("\n:     |       |        :");
+    printf("\n:    /|\\      |        :");
+    printf("\n:   / | \\     |        :");
+    printf("\n:     |       |        :");
+    printf("\n:     /\\     / \\       :");
+    printf("\n:    /  \\   /   \\      :");
+    printf("\n:          /     \\     :");
+    printf("\n\n    \4  %s  \4\n\n",bWordCoppy);
 
 
-
+    printf("\n ::Your guys is dead. Player 1 wins!!!:: \n\n");
 
 
 
